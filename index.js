e5ca80c9d2de60e9ccbb848720d048bfc2196c4f
Allan De Melo Correa <127619200+allancorreaz@users.noreply.github.com>
2024-08-25 06:48:36 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let resultado = calcularNivel(vitorias = 90, derrotas = 10); // resultado da conta 
 console.log(`O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`); // o ponto após o resultado é parametro para juntar os dois valores no console.log
 function calcularNivel(vitorias, derrotas){ // parametro da function
-    let saldoVitorias = vitorias - derrotas // subtração para o let acima
+    let saldoVitorias = vitorias - derrotas // subtração para o let acima e variável guardada devido ao return
     let nivel; // variaveis guardadas devido ao return
 
     if(vitorias < 10){
